17d23690862903c156f1401421e94e30259fb62a
Viktoria <zaharova181114@gmail.com>
2022-07-10 21:06:02 +0300

    add community

--- app/js/common.js
+++ app/js/common.js
@@ -251,6 +251,16 @@ $('.product-gallery-preview').slick({
     ]
 });
 
+$('.reviews-slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    fade: true,
+    appendDots: '.reviews-slider__nav',
+    appendArrows: '.reviews-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+});
+
 // показать карточку товара по наведению в зависимости от значениея data-tab
 $(document).ready(function () {
     $(".js-tab-trigger").hover(function () {
